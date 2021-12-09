bf2c472c4343b8355f94fbb58ef5f25b2562f154
Tompa0 <tominac.tomislav43@gmail.com>
2021-12-09 19:33:47 +0100

    test2

--- test/specs/basic.e2e.js
+++ test/specs/basic.e2e.js
@@ -1,6 +1,20 @@
-describe('Open Wdio Website', () => {
-    it('Proper title is displayed', () => {
-        browser.url('https://webdriver.io/');
-        expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
+describe('Shoutem Web Stranica', () => {
+    it('Ispisuje ispravan h1 naslov', async () => {
+        browser.url('https://shoutem.com/pricing/');
+
+        //Definiranje
+        const naslov = await $('h1');
+        await expect(naslov).toHaveText('There are more ways to create apps with Shoutem');
+
     })
+
+    it('Ispisuje ispravan h1 naslov', async () => {
+        browser.url('https://shoutem.com/apps/social-network/');
+        const gumb = await $('a=Build now');
+        gumb.click();
+        const iduciNaslov = await $('h1');
+        await expect(iduciNaslov).toHaveText('Choose an app template');
+
+
+    });
 })
