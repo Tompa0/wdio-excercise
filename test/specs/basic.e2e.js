84e6bb7f387e8f3d678135452dd668ae9aeb6fc1
Tompa0 <tominac.tomislav43@gmail.com>
2021-12-23 20:10:33 +0100

    Update basic.e2e.js

--- test/specs/basic.e2e.js
+++ test/specs/basic.e2e.js
@@ -1,6 +1,36 @@
-describe('Open Wdio Website', () => {
-    it('Proper title is displayed', () => {
-        browser.url('https://webdriver.io/');
-        expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
-    })
+describe('OpenMRS stranica', () => {
+    it('Prijava sa netočnim podacima', async () => {
+        browser.url("https://demo.openmrs.org/openmrs/login.htm");
+
+        const usernameInput = await $('#username');
+        const passwordInput = await $('#password');
+        const pharmacyButton = await $('#Laboratory');
+        const loginButton = await $('#loginButton');
+
+        await usernameInput.addValue('user');
+        await passwordInput.addValue('password');
+        await pharmacyButton.click();
+        await loginButton.click();
+
+        const errorMessage = await $('#error-message');
+        await expect(errorMessage).toHaveText("Invalid username/password. Please try again.");
+    });
+
+
+    it('Prijava sa točnim podacima', async () => {
+        browser.url("https://demo.openmrs.org/openmrs/login.htm");
+
+        const usernameInput = await $('#username');
+        const passwordInput = await $('#password');
+        const pharmacyButton = await $('#Laboratory');
+        const loginButton = await $('#loginButton');
+
+        await usernameInput.addValue('Admin');
+        await passwordInput.addValue('Admin123');
+        await pharmacyButton.click();
+        await loginButton.click();
+
+        const h4 = await $('h4');
+        await expect(h4).toHaveText("Logged in as Super User (admin) at Laboratory.");
+    });
 })
