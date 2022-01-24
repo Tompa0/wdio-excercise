c49a448dbf4df1a13fde48d8b1259f4669cb833d
Tompa0 <tominac.tomislav43@gmail.com>
2022-01-24 23:47:00 +0100

    Update basic.e2e.js

--- test/specs/basic.e2e.js
+++ test/specs/basic.e2e.js
@@ -1,6 +1,21 @@
-describe('Open Wdio Website', () => {
-    it('Proper title is displayed', () => {
-        browser.url('https://webdriver.io/');
-        expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
+describe('Coinmarket Cap stranica', () => {
+
+
+
+    it('Ispisuje ispravan h3 Bitcoin naslov', async () => {
+        browser.url('https://coinmarketcap.com/currencies/bitcoin');
+
+        const naslovBtc = await $('h3');
+        await expect(naslovBtc).toHaveText('Bitcoin to USD Chart');
+
     })
+
+    it('Ispisuje ispravan h3 Ethereum naslov', async () => {
+        browser.url('https://coinmarketcap.com/currencies/ethereum/');
+
+        const naslovEth = await $('h3');
+        await expect(naslovEth).toHaveText('Ethereum to USD Chart');
+
+    })
+
 })
